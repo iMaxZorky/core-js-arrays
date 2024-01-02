8ce8866a2e4baf8226fc86174a3e712f7b4e7494
iMaxZorky <mkoprav@gmail.com>
2024-01-02 13:50:38 +0600

    feat: 19 tasks done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -325,8 +325,12 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return new Array(Math.ceil(arr.length / chunkSize))
+    .fill(0)
+    .map((value, index) =>
+      arr.slice(index * chunkSize, index * chunkSize + chunkSize)
+    );
 }
 
 /**
