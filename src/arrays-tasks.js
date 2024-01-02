f952a56ff278f1e748f8d827d1465c1317ccc741
iMaxZorky <mkoprav@gmail.com>
2024-01-02 17:04:46 +0600

    feat: 21 tasks done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -122,8 +122,9 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = arr.reduce((acc, number) => acc + number, 0);
+  return +(sum / arr.length).toFixed(2) || 0;
 }
 
 /**
