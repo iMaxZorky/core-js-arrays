6a990ae797a1eeef02beeed027419456e56fa627
iMaxZorky <mkoprav@gmail.com>
2024-01-02 14:14:11 +0600

    feat: 20 tasks done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -260,8 +260,10 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n === 0) return [];
+  const arr = new Array(size).fill(0);
+  return n === 1 ? arr : arr.map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
